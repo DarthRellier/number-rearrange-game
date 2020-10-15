9bb6a512cbbf1438889eeffaba43e5768e03c29b
DarthRellier <71454911+DarthRellier@users.noreply.github.com>
2020-10-15 10:24:12 -0700

    Mabye added cheat and mabye added a code.

--- main.js
+++ main.js
@@ -203,3 +203,19 @@ function randomizeBoard() {
   }
   updateView();
 }
+
+function cheat() {
+  let code = document.getElementById("code");
+  let length = "i will use this for display ;)";
+  if (code.value.toLowerCase() == length) {
+    model = [
+      [1, 6, 11, 16, 21],
+      [2, 7, 12, 17, 22],
+      [3, 8, 13, 18, 23],
+      [4, 9, 14, 19, 24],
+      [5, 10, 15, 20, null],
+    ];
+    updateView();
+  }
+}
+// FOR DISPLAY ONLY
